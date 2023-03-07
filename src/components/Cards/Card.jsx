190347fa2dab006d6219eed77ee70639e728dc68
Clement Duchemin <clementduchemin19@gmail.com>
2023-03-07 12:57:17 +0100

    gh-pages test 4

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,14 +1,28 @@
-function Card({ cover, title}) {
+import { Link } from "react-router-dom";
+
+function Card({id, cover, title}) {
   return (
-    <article className="card-logement">
+    <Link to={`/logement/${id}`} className="card-logement">
         <img src={cover} alt="appartement" />
         <div className="card-logement__layer">
             <p className="card-logement__title">{title}</p>
         </div>
-    </article>
+    </Link>
   );
 }
 
 export default Card
 
 
+// function Card({ cover, title}) {
+//   return (
+//     <article className="card-logement">
+//         <img src={cover} alt="appartement" />
+//         <div className="card-logement__layer">
+//             <p className="card-logement__title">{title}</p>
+//         </div>
+//     </article>
+//   );
+// }
+
+// export default Card
