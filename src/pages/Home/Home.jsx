01d55253173deff80622d11af15b9389ff0af614
Clement Duchemin <clementduchemin19@gmail.com>
2023-03-07 13:57:41 +0100

    gh-pages test 5

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,60 +3,60 @@ import Banner from "../../components/Banner/Banner";
 import Card from "../../components/Cards/Card";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import datas from '../../data/data'
-
-// function Home() {
-//     const [data, setData] = useState([]);
-
-//     useEffect(() => {
-//         axios.get("/logements.json")
-//         .then((res) => setData(res.data));
-//     }, []);
-
-//     return (
-//         <>
-//             <Banner />
-//             <div className="cards-container">
-//                 {data.map((flat, id) => (
-//                     <div className="card_logement" key={id}>
-//                         <Link className="link_card_logement" to={`/logement/${flat.id}`}>
-//                             <Card cover={flat.cover} title={flat.title} />
-//                         </Link>
-//                     </div>
-//                 ))}
-//             </div>
-//         </>
-//     );
-// }
-
-// export default Home
-
+//import datas from '../../data/data'
 
 function Home() {
-    
+    const [data, setData] = useState([]);
 
-    
+    useEffect(() => {
+        axios.get("https://raw.githubusercontent.com/clement-duchemin/kasa/master/public/logements.json")
+        .then((res) => setData(res.data));
+    }, []);
 
     return (
         <>
             <Banner />
             <div className="cards-container">
-            {datas.map(data => {
-                return (
-                    <Card
-                        key={data.id}
-                        id={data.id}
-                        title={data.title}
-                        cover={data.cover}
-                    />
-                )
-            })}
+                {data.map((flat, id) => (
+                    <div className="card_logement" key={id}>
+                        <Link className="link_card_logement" to={`/logement/${flat.id}`}>
+                            <Card cover={flat.cover} title={flat.title} />
+                        </Link>
+                    </div>
+                ))}
             </div>
         </>
     );
 }
 
 export default Home
+
+
+// function Home() {
+    
+
+    
+
+//     return (
+//         <>
+//             <Banner />
+//             <div className="cards-container">
+//             {datas.map(data => {
+//                 return (
+//                     <Card
+//                         key={data.id}
+//                         id={data.id}
+//                         title={data.title}
+//                         cover={data.cover}
+//                     />
+//                 )
+//             })}
+//             </div>
+//         </>
+//     );
+// }
+
+// export default Home
 
 
 
