9403a9c0bad7b1d8f5cc3cab6afdec7c0839b41f
Clement Duchemin <clementduchemin19@gmail.com>
2023-03-07 14:46:24 +0100

    error

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -5,7 +5,7 @@ function ErrorPage() {
     <div className="errorContainer">
         <p className="errorNumber">404</p>
         <p className="errorText">Oups! La page que vous demandez n'existe pas.</p>
-        <Link to="/" className="errorLinkHome">
+        <Link to="/kasa/" className="errorLinkHome">
             Retourner sur la page d'accueil
         </Link>
     </div>
