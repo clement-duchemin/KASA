f8e7f92a99999fd94defbeff5bba964f12f226cc
Clement Duchemin <clementduchemin19@gmail.com>
2023-03-08 16:35:49 +0100

    comments

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -38,39 +38,41 @@ import Chevron from "../../assets/chevron.svg";
 
 // export default Collapse
 
-export default function Collapse(props) {
-	const [toggle, setToggle] = useState(false); // je definie le state du toggle (et false par défaut)
+ function Collapse(props) {
+	const [toggle, setToggle] = useState(false); // je definie la valeur du state du toggle à false.
 	const [heightEl, setHeightEl] = useState(); // je definie le state de la hauteur du collapse
 
 	const toggleState = () => {
-		//je définie la fonction toggleState qui modifie la valeur toggle au clic
+		//je définie la fonction toggleState qui modifie la valeur toggle au clic.
 		setToggle(!toggle);
 	};
 
-	const refHeight = useRef(); //récupère et conserve la valeur de hauteur du collapse déplié
+	const refHeight = useRef(); //La méthode useRef, ici déclarée, permettra de récupérer et conserver la valeur de la hauteur du collapse déplié.
 
 	useEffect(() => {
 		setHeightEl(`${refHeight.current.scrollHeight}px`); //useEffect s'éxécute au montage du composant, dans ce cas, il définit la hauteur du collapse déplié lors de sa première ouverture et la conserve dans refHeight
-	}, []);
+	}, []); //La propriété scrollHeight permet de mesurer la hauteur de l'élément, y compris ce qui n'est pas visible(overflow).
 
 	return (
 		// affiche le collapse replié par défaut et l'ouvre au clic puis le referme au clic en faisant disparaitre le texte et le style
-		<div className={`collapse ${props.aboutStyle}`}>
+		<div className={`collapse`}>
 			<div onClick={toggleState} className="collapse__visible">
 				<h2>{props.aboutTitle}</h2>
 				<img
-					className={toggle ? "chevron rotated" : "chevron"}
+					className={toggle ? "chevron rotated" : "chevron"}//Au premier clic, les classes chevron et rotate sont activées, au deuxième seulement chevron.
 					src={Chevron}
 					alt="chevron"
 				/>
 			</div>
 			<div
-				ref={refHeight}
+				ref={refHeight} // La constante refHeight, qui contient la logique de capture de la hauteur du collapse est passée en référence.
 				className={toggle ? "collapse__toggle animated" : "collapse__toggle"}
 				style={{ height: toggle ? `${heightEl}` : "0px" }}
 			>
-				<p aria-hidden={toggle ? "true" : "false"}>{props.aboutText}</p>
-			</div>
+				<p>{props.aboutText}</p> 
+			</div> 
 		</div>
-	);
+	); 
 }
+
+export default Collapse
--- src/components/Rate/Rate.jsx
+++ src/components/Rate/Rate.jsx
@@ -1,21 +1,21 @@
 import fullStar from "../../assets/full-star.svg"
 import emptyStar from "../../assets/empty-star.svg"
 
-function Rate({ score }) {
+function Rate({ score }) {  // Le score est ici passé en props.
     const stars = [1, 2, 3, 4, 5];
   return (
     <div className="rate-stars">
         {stars.map((level) => 
-            score >= level ? (
+            score >= level ? (     // Affiche-moi les levels (étoiles rouges) inférieurs et égaux au score.
                 <img
-                    key={level.toString()}
-                    className="star"
+                    key={level.toString()}// Les keys dans les tableaux et listes doivent être uniques. Ici je passe l'élément de la méthode map.
+                    className="star"       
                     src={fullStar}
                     alt="rating stars"
                 />    
             ) : (
                 <img
-                    key={level.toString()}
+                    key={level.toString()} // Les levels (empty stars) supérieurs au score seront affiché en gris.
                     className="star"
                     src={emptyStar}
                     alt="rating stars"
